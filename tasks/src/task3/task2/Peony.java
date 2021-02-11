d6fe14b263a017f7e791631e8e8d010919e02a05
Sergey Kurtsevich <wipe2sky@gmail.com>
2021-02-11 18:49:51 +0300

    Task_3_2

---
+++ tasks/src/task3/task2/Peony.java
@@ -0,0 +1,14 @@
+package task3.task2;
+
+public class Peony extends Flower {
+    private String name = "Peony";
+    private int cost = 11;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
---
+++ tasks/src/task3/task2/Rose.java
@@ -0,0 +1,14 @@
+package task3.task2;
+
+public class Rose extends Flower {
+    private String name = "Rose";
+    private int cost = 10;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
