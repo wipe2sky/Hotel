a4382c67f74df34f39f6ee2d7be62bb036ff41f0
Sergey Kurtsevich <wipe2sky@gmail.com>
2021-03-04 18:34:44 +0300

    Delete another project

--- tasks/src/task3/task3/Engine.java
+++
@@ -1,4 +0,0 @@
-package task3.task3;
-
-public class Engine implements IProductPart{
-}
--- tasks/src/task3/task3/IAssemblyLine.java
+++
@@ -1,5 +0,0 @@
-package task3.task3;
-
-public interface IAssemblyLine {
-    IProduct assemblyProduct(IProduct product);
-}
